1dced6531a5a9eff0f71e08aa98aba5aae51fed5
Gregory Ryan <gwryan@buffalo.edu>
2021-06-07 09:55:03 -0400

    ishortestWord implemented using the speard operator in order to allow the mMath.min method to access all elements of the mapped array as individual arguments.

--- shortestWord.js
+++ shortestWord.js
@@ -1,10 +1,16 @@
+// function findShort(s){
+//     var min = Number.MAX_SAFE_INTEGER;
+//     var sArray = s.split(' ');
+//     sArray.forEach((element) => { 
+//         if (element.length < min) min = element.length;
+//     });
+//     return min;
+// }
+
+
+// ... is the spread operator. Allows arrays, strings or objects to be expanded.
 function findShort(s){
-    var min = Number.MAX_SAFE_INTEGER;
-    var sArray = s.split(' ');
-    sArray.forEach((element) => { 
-        if (element.length < min) min = element.length;
-    });
-    return min;
+    return Math.min(...s.split(" ").map (s => s.length));
 }
 
 console.log(findShort("Let's travel abroad shall we"));
