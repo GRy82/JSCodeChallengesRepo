442ade78d884b7158c4d103c04fee7179add6770
Gregory Ryan <gwryan@buffalo.edu>
2021-06-07 13:12:16 -0400

    Implementation improved with the use of the .lastIndexOf array method.

--- duplicateEncode.js
+++ duplicateEncode.js
@@ -1,13 +1,20 @@
+// function duplicateEncode(word){
+//     var output = "";
+//     var wordArray = word.toLowerCase().split('');
+//     wordArray.forEach(element => {
+//         let tempArray = wordArray.slice();
+//         tempArray.splice(wordArray.indexOf(element), 1);
+//         if(tempArray.includes(element)) output += ')';
+//         else output += '(';
+//     }); 
+//     return output;
+// }
+
 function duplicateEncode(word){
-    var output = "";
-    var wordArray = word.toLowerCase().split('');
-    wordArray.forEach(element => {
-        let tempArray = wordArray.slice();
-        tempArray.splice(wordArray.indexOf(element), 1);
-        if(tempArray.includes(element)) output += ')';
-        else output += '(';
-    }); 
-    return output;
+    return word.toLowerCase().split('').map(element => {
+        return word.indexOf(element) == word.lastIndexOf(element) ? 
+            '(' : ')';
+    }).join('');
 }
 
 console.log(duplicateEncode("Occurrences"));
