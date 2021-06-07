224d1579603d0615e79bcdafc41a4c414e2d7233
Gregory Ryan <gwryan@buffalo.edu>
2021-06-07 09:17:01 -0400

    solution refactored using array.reduce() method.

--- busStops.js
+++ busStops.js
@@ -1,12 +1,13 @@
 busStops = [[10,0],[3,5],[5,8]];
 
-var number = function(busStops){
-    var occupants = 0;
-    busStops.forEach(element => {
-        occupants += element[0] - element[1];
-    });
-    return occupants;
-};
+// var number = function(busStops){
+//     var occupants = 0;
+//     busStops.forEach(element => {
+//         occupants += element[0] - element[1];
+//     });
+//     return occupants;
+// };
 
+var number = busStops.reduce((occupants, element) => { return occupants + (element[0] - element[1])}, 0);
 
-console.log(number(busStops));
+console.log(number);
