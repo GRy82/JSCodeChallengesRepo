7d2b0b3a72be9073482f354178e2db9dbcbbd095
Gregory Ryan <gwryan@buffalo.edu>
2021-06-13 15:07:36 -0400

    Better solution reached, but still does not work for all appropriate use cases.

--- decomposeSquare.js
+++ decomposeSquare.js
@@ -1,29 +1,83 @@
 function decompose(n) {
-    return buildDecomposed(Math.pow(n, 2), new Array(), n - 1);
-}
+    let currentTestedDigit = n - 1;
+    let remainingPortion = Math.pow(n, 2);
+    let digitsArray = [];
 
-function buildDecomposed(remainingPortion, digitsArray, currentTested){
-    if(remainingPortion === 0) 
-        return digitsArray;
+    while(remainingPortion > 0){
+        let possibleSolution = buildDecomposed(remainingPortion, [], currentTestedDigit);
 
-    let possibleRemainder = remainingPortion - Math.pow(currentTested, 2);
+        if(!containsRepeats(possibleSolution) && possibleSolution[0] > 0)
+            digitsArray.unshift(currentTestedDigit);
 
-    if (possibleRemainder < 0) 
-        return buildDecomposed(remainingPortion, digitsArray, currentTested - 1);
-    
-    digitsArray.unshift(currentTested);    
+        remainingPortion -= Math.pow(currentTestedDigit, 2);
 
-    if(possibleRemainder === 0)
-        return digitsArray;
-
-    let possibleNewDigitsArray = buildDecomposed(possibleRemainder, digitsArray, --currentTested);
-
-    if(possibleNewDigitsArray[0] < 1){
-        digitsArray.shift();
-        return buildDecomposed(possibleRemainder, digitsArray, currentTested - 1);
+        currentTestedDigit = remainingPortion - Math.pow(currentTestedDigit - 1, 2) < 0 ? 
+            nextLowestValidDigit(remainingPortion) : currentTestedDigit - 1;
     }
 
-    return possibleNewDigitsArray;
+    return digitsArray;
 }
 
-console.log(decompose(11));
+
+function buildDecomposed(remainingPortion, testArray, currentTestedDigit){
+    if(remainingPortion === 0)
+        return testArray;
+    
+    currentTestedDigit = remainingPortion - Math.pow(currentTestedDigit, 2) < 0 ? 
+        nextLowestValidDigit(remainingPortion) : currentTestedDigit;
+
+    remainingPortion -= Math.pow(currentTestedDigit, 2);
+
+    testArray.unshift(currentTestedDigit);
+
+    return buildDecomposed(remainingPortion, testArray, currentTestedDigit - 1);
+}
+
+
+function containsRepeats(arr){
+    let newArray = [];
+    for(var value in arr){
+            if(newArray.includes(arr[value])) return true;
+            newArray.push(arr[value]);
+        }
+        return false;
+}
+        
+
+function nextLowestValidDigit(remainingPortion){
+    return Math.floor(Math.pow(remainingPortion, .5));
+}
+
+console.log(decompose(12));
+
+// function decompose(n) {
+//     let finalArray = [];
+//     finalArray = buildDecomposed(Math.pow(n, 2), [], n - 1);
+//     return finalArray;
+// }
+
+// function buildDecomposed(remainingPortion, digitsArray, currentTested){
+//     if(remainingPortion === 0) 
+//         return digitsArray;
+
+//     let possibleRemainder = remainingPortion - Math.pow(currentTested, 2);
+
+//     if (possibleRemainder < 0) {
+//         currentTested = nextLowestValidDigit(remainingPortion);
+//         return buildDecomposed(remainingPortion, digitsArray, currentTested);
+//     }
+    
+//     digitsArray.unshift(currentTested);    
+
+//     if(possibleRemainder === 0)
+//         return digitsArray;
+
+//     let possibleNewDigitsArray = buildDecomposed(possibleRemainder, digitsArray, --currentTested);
+
+//     if(possibleNewDigitsArray[0] < 1){
+//         digitsArray.shift();
+//         return buildDecomposed(possibleRemainder, digitsArray, currentTested - 1);
+//     }
+
+//     return possibleNewDigitsArray;
+// }
