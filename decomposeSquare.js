8de2b3ff02624f2ad4d30f6c4ab8e9c92685eb79
Gregory Ryan <gwryan@buffalo.edu>
2021-06-14 17:35:03 -0400

    comments added.

--- decomposeSquare.js
+++ decomposeSquare.js
@@ -2,48 +2,51 @@ var invalidRemainders = new Set([2, 3, 6, 7, 8, 11, 12, 15, 18, 19, 22, 23]);
 
 function decompose(n){
     if(!n) return null;
-    let solution = buildArray(Math.pow(n, 2), [], n - 1);
+    // start with remainder of n^2. start with n - 1 as first digit to check as part of solution
+    let solution = buildArray(Math.pow(n, 2), [], n - 1); 
+    //if solution contains any 0's or negative integers, it is not a valid input, and thus return null.
     if(solution.some(c => c <= 0)) return null;
     else return solution;
 }
 
 function buildArray(remainder, testArray, testedDigit){
-    if(remainder === 0) return testArray;
+    if(remainder === 0) return testArray; //you have found a valid solution or invalid solution given invalid input.
 
-    possibleRemainder = remainder - Math.pow(testedDigit, 2); 
+    possibleRemainder = remainder - Math.pow(testedDigit, 2); //this is what the remainder will be sent as to the next func call...
 
-    if (isDeadEnd(possibleRemainder, testedDigit))
-        return buildArray(remainder, testArray.slice(), nextLowestValidDigit(remainder, testedDigit));
+    if (isDeadEnd(possibleRemainder, testedDigit))//... if it is not a dead end leading to invalid solution.
+        return buildArray(remainder, testArray.slice(), nextLowestValidDigit(remainder, testedDigit)); //if deadend, try with next valid number, and original remainder.
 
-    testArray.unshift(testedDigit); 
+    testArray.unshift(testedDigit); //testedDigit was not a deadend. Add to collection and proceed recursively.
     return  buildArray(possibleRemainder, testArray.slice(), nextLowestValidDigit(possibleRemainder, testedDigit));
 
 }
                   
 function isDeadEnd(remainder, testedDigit){
-    if(remainder === 0) return false;
-    if(invalidRemainders.has(remainder))
+    if(remainder === 0) return false; //If you are at the end of the solution.
+    if(invalidRemainders.has(remainder)) //If remainder is impossible to resolve.
         return true;
     
     let nextDigit = nextLowestValidDigit(remainder, testedDigit);
-    for(let i = nextDigit; i > 0; i--){
+    for(let i = nextDigit; i > 0; i--){ //starting at valid digit whose square is less than the remainder, and moving down.
         let tempRemainder = remainder;
-        if(invalidRemainders.has(remainder - i * i)) continue;
-        for(let j = i; j > 0; j--){
+        if(invalidRemainders.has(remainder - i * i)) continue; //this is a deadEnd, move to next possibility.  
+        for(let j = i; j > 0; j--){ //See if remainder can even possibly get down to 0...
             tempRemainder -= j * j;
         }
-        if(tempRemainder > 0) 
+        if(tempRemainder > 0) //... if it can't continue to next iteration
             continue;
         else 
-            return false;
+            return false; // if you get to here, it't not a dead end.
     }
 
-    invalidRemainders.add(remainder);
+    invalidRemainders.add(remainder); //if you ge through entire loop, add to the invalidRemainders set, and return true.
     return true;
 }
 
-
-function nextLowestValidDigit(remainingPortion, currentTestedDigit){
+// returns currentDigit decremented by one.. Or if that number squared is larger than the remainder, 
+// skip to a lower number -- the highest number that, when squared, is less than the remainder.
+function nextLowestValidDigit(remainingPortion, currentTestedDigit){ 
     if(remainingPortion - Math.pow(currentTestedDigit - 1, 2) >= 0)
         return currentTestedDigit - 1;
     
