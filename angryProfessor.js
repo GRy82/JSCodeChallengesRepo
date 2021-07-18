467c5f074e857cae9add8d3036f115dd36543d86
Gregory Ryan <gwryan@buffalo.edu>
2021-07-18 10:43:18 -0400

    Coding challenge completed. See the added module for the solution, and the comments above for a description of the challenge.

---
+++ angryProfessor.js
@@ -0,0 +1,12 @@
+// A Discrete Mathematics professor has a class of students. Frustrated with their lack 
+// of discipline, the professor decides to cancel class if fewer than some number of 
+// students are present when class starts. Arrival times go from on time (arrivalTime <= 0) 
+// to late (arrivalTime > 0).
+
+
+// Given the arrival time of each student and a threshhold number of attendees, determine 
+// if the class is cancelled.
+
+function angryProfessor(k, a) {
+    return a.filter(c => c <= 0).length < k ? 'YES' : 'NO';
+}
